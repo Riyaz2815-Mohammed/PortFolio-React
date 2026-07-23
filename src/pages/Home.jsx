6578f16b4fd34efc5bd1005928bcf7fd33176726
Riyaz2815-Mohammed <anvarbhashaj1ace@gmail.com>
2026-07-23 12:05:18 +0530

    feat: Added Navbar and Home page and introduced styles for it

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css";
+function Home() {
+  return (
+    <main className="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>I am a Software Engineer.</p>
+    </main>
+  );
+}
+
+export default Home;
